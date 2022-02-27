dae82ea35440751eae7772b1a7349099b35a6ead
GrandTheftGelato <colintso@hotmail.com>
2022-02-28 02:04:21 +1100

    Added description of technologies used for the personal portfolio page

--- dist/ajax.js
+++ dist/ajax.js
@@ -1,7 +1,7 @@
 var htmlContent = $.getJSON("project-tiles.json", function () {
     console.log("success");
 }).done(function (data) {
-    var resultHTML = projectTiles(data);
+    var pugHTML = projectTiles(data);
     //console.log(resultHTML);
-    $(".projects-tile-container").append(resultHTML);
+    $(".projects-tile-container").append(pugHTML);
 });
